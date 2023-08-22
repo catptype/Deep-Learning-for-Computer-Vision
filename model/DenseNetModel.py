e65b3a6c54548300693db009c17dcec207a8823d
Cat <cattypkung@gmail.com>
2023-08-22 20:25:54 +0700

    Update DenseNetModel.py

--- model/DenseNetModel.py
+++ model/DenseNetModel.py
@@ -1,4 +1,21 @@
+"""
+DenseNet Models
+~~~~~~~~~~~~~~~~
+
+This module defines a set of DenseNet architectures for image classification tasks using TensorFlow and Keras.
+
+Classes:
+    - DenseNetModel: Base class for DenseNet architectures.
+    - DenseNet121: Implementation of DenseNet-121 architecture.
+    - DenseNet169: Implementation of DenseNet-169 architecture.
+    - DenseNet201: Implementation of DenseNet-201 architecture.
+    - DenseNet264: Implementation of DenseNet-264 architecture.
+
+Note: To use these models, TensorFlow and Keras must be installed.
+"""
+
 import sys
+sys.dont_write_bytecode = True
 
 import tensorflow as tf
 from tensorflow.keras.layers import (
@@ -13,21 +30,36 @@ from tensorflow.keras.layers import (
     ReLU,
 )
 from tensorflow.keras.models import Model
-
 from .DeepLearningModel import DeepLearningModel
-
-sys.dont_write_bytecode = True
 
 
 class DenseNetModel(DeepLearningModel):
+    """
+    Base class for DenseNet architectures.
+    
+    Attributes:
+        - growth_rate: The growth rate for the DenseNet model.
+    
+    Methods:
+        - init_block(input): Initializes the initial block of the DenseNet model.
+        - BN_ReLU_Conv(input, num_feature, kernel): Applies BatchNormalization, ReLU activation, and Convolution layers.
+        - Dense_block(input, num_feature, num_layer): Creates a dense block with a given number of layers.
+        - Transit_block(input): Creates a transition block with AveragePooling.
+    """
     def __init__(self, image_size, num_classes, **kwargs):
+        """
+        Initializes the DenseNet model with specified parameters.
+        
+        Args:
+            - image_size (int): The input image size.
+            - num_classes (int): The number of output classes.
+            - **kwargs: Additional keyword arguments.
+        """
         self.growth_rate = kwargs["growth_rate"]
         super().__init__(image_size=image_size, num_classes=num_classes)
 
     def init_block(self, input):
-        x = Conv2D(
-            64, (7, 7), strides=2, padding="same", kernel_initializer="he_normal"
-        )(input)
+        x = Conv2D(64, (7, 7), strides=2, padding="same", kernel_initializer="he_normal")(input)
         x = BatchNormalization()(x)
         x = ReLU()(x)
         x = MaxPooling2D((3, 3), strides=2, padding="same")(x)
@@ -59,10 +91,27 @@ class DenseNetModel(DeepLearningModel):
 
 
 class DenseNet121(DenseNetModel):
+    """
+    Implementation of DenseNet-121 architecture.
+    """
     def __init__(self, image_size, num_classes, growth_rate=32):
+        """
+        Initializes the DenseNet-121 model with specified parameters.
+        
+        Args:
+            - image_size (int): The input image size.
+            - num_classes (int): The number of output classes.
+            - growth_rate (int): The growth rate for the model.
+        """
         super().__init__(image_size=image_size, num_classes=num_classes, growth_rate=growth_rate)
 
     def build_model(self):
+        """
+        Builds the DenseNet-121 model.
+        
+        Returns:
+            - model: The built Keras model.
+        """
         # Input layer
         input = Input(shape=(self.image_size, self.image_size, 3), name="Input_image")
         x = self.init_block(input)
@@ -87,10 +136,27 @@ class DenseNet121(DenseNetModel):
 
 
 class DenseNet169(DenseNetModel):
+    """
+    Implementation of DenseNet-169 architecture.
+    """
     def __init__(self, image_size, num_classes, growth_rate=32):
+        """
+        Initializes the DenseNet-169 model with specified parameters.
+        
+        Args:
+            - image_size (int): The input image size.
+            - num_classes (int): The number of output classes.
+            - growth_rate (int): The growth rate for the model.
+        """
         super().__init__(image_size=image_size, num_classes=num_classes, growth_rate=growth_rate)
 
     def build_model(self):
+        """
+        Builds the DenseNet-169 model.
+        
+        Returns:
+            - model: The built Keras model.
+        """
         # Input layer
         input = Input(shape=(self.image_size, self.image_size, 3), name="Input_image")
         x = self.init_block(input)
@@ -115,10 +181,27 @@ class DenseNet169(DenseNetModel):
 
 
 class DenseNet201(DenseNetModel):
+    """
+    Implementation of DenseNet-201 architecture.
+    """    
     def __init__(self, image_size, num_classes, growth_rate=32):
+        """
+        Initializes the DenseNet-169 model with specified parameters.
+        
+        Args:
+            - image_size (int): The input image size.
+            - num_classes (int): The number of output classes.
+            - growth_rate (int): The growth rate for the model.
+        """
         super().__init__(image_size=image_size, num_classes=num_classes, growth_rate=growth_rate)
 
     def build_model(self):
+        """
+        Builds the DenseNet-201 model.
+        
+        Returns:
+            - model: The built Keras model.
+        """
         # Input layer
         input = Input(shape=(self.image_size, self.image_size, 3), name="Input_image")
         x = self.init_block(input)
@@ -143,10 +226,27 @@ class DenseNet201(DenseNetModel):
 
 
 class DenseNet264(DenseNetModel):
+    """
+    Implementation of DenseNet-264 architecture.
+    """    
     def __init__(self, image_size, num_classes, growth_rate=32):
+        """
+        Initializes the DenseNet-169 model with specified parameters.
+        
+        Args:
+            - image_size (int): The input image size.
+            - num_classes (int): The number of output classes.
+            - growth_rate (int): The growth rate for the model.
+        """
         super().__init__(image_size=image_size, num_classes=num_classes, growth_rate=growth_rate)
 
     def build_model(self):
+        """
+        Builds the DenseNet-264 model.
+        
+        Returns:
+            - model: The built Keras model.
+        """
         # Input layer
         input = Input(shape=(self.image_size, self.image_size, 3), name="Input_image")
         x = self.init_block(input)
