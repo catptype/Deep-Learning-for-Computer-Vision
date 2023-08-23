933aeebf19841aebc59a432c48f0f01b65b0eaf3
Cat <cattypkung@gmail.com>
2023-08-23 19:43:38 +0700

    Implement CBAM module

--- model/Res2NetModel.py
+++ model/Res2NetModel.py
@@ -17,12 +17,14 @@ sys.dont_write_bytecode = True
 
 import tensorflow as tf
 from tensorflow.keras.layers import (
+    Activation,
     Add,
     BatchNormalization,
     Concatenate,
     Conv2D,
     Dense,
     GlobalAveragePooling2D,
+    GlobalMaxPool2D,
     Input,
     MaxPooling2D,
     Multiply,
@@ -47,7 +49,7 @@ class Res2NetModel(DeepLearningModel):
         - SE_block(input, identity, ratio): Creates a Squeeze-and-Excitation (SE) block.
         - Residual_bottleneck(input, num_feature): Creates a Residual Bottleneck block.
     """
-    def __init__(self, image_size, num_classes, **kwargs):
+    def __init__(self, image_size, num_classes, scale, module):
         """
         Initializes the Res2Net model with specified parameters.
         
@@ -56,8 +58,9 @@ class Res2NetModel(DeepLearningModel):
             - num_classes (int): The number of output classes.
             - **kwargs: Additional keyword arguments.
         """
-        self.scale = kwargs["scale"]
-        self.use_se = kwargs["use_se"]
+        assert module is None or module == 'se' or module == 'cbam', f"module value must be 1:None, 2:'se', 3:'cbam', current ({module})"
+        self.scale = scale
+        self.module = module
         super().__init__(image_size=image_size, num_classes=num_classes)
 
     def Conv2D_block(self, input, num_feature, kernel=3, strides=1, use_skip=False, identity=None):
@@ -70,6 +73,52 @@ class Res2NetModel(DeepLearningModel):
         x = ReLU()(x)
         return x
     
+    def Channel_Attention(self, input, ratio=16):
+        num_channel = input.shape[-1]
+        
+        # Shared layers
+        w0 = Dense(num_channel // ratio, activation='relu', kernel_initializer='he_normal')
+        w1 = Dense(num_channel, kernel_initializer='he_normal')
+
+        # Global Max Pool
+        GMP_pool = GlobalMaxPool2D()(input)
+        GMP_pool = w0(GMP_pool)
+        GMP_pool = w1(GMP_pool)
+
+        # Global Average Pool
+        GAP_pool = GlobalAveragePooling2D()(input)
+        GAP_pool = w0(GAP_pool)
+        GAP_pool = w1(GAP_pool)
+   
+        channel_attention = Add()([GMP_pool, GAP_pool])
+        channel_attention = Activation('sigmoid')(channel_attention)
+
+        output = Multiply()([input, channel_attention])
+        
+        return output
+    
+    def Spatial_Attention(self, input):
+
+        # Max pool 
+        max_pool = tf.reduce_max(input, axis=-1)
+        max_pool = tf.expand_dims(max_pool, axis=-1)
+        
+        # Average pool
+        avg_pool = tf.reduce_mean(input, axis=-1)
+        avg_pool = tf.expand_dims(avg_pool, axis=-1)
+        
+        spatial_attention = Concatenate()([max_pool, avg_pool])
+        spatial_attention = Conv2D(1, (7,7), padding='same', activation='sigmoid', kernel_initializer='he_normal')(spatial_attention)
+
+        output = Multiply()([input, spatial_attention])
+        return output
+    
+    def CBAM(self, input, ratio=16):
+        x = self.Channel_Attention(input, ratio)
+        x = self.Spatial_Attention(x)
+        return x
+    
+
     def Residual_bottleneck(self, input, num_feature):
         module_output = []
 
@@ -91,12 +140,19 @@ class Res2NetModel(DeepLearningModel):
 
         x = Concatenate()(module_output)
 
-        if self.use_se:
+        if self.module is None:
+            x = self.Conv2D_block(x, num_feature, kernel=1, use_skip=True, identity=input)
+
+        elif self.module == 'se':
             x = self.Conv2D_block(x, num_feature, kernel=1)
             with tf.name_scope("SE_Module"):
                 x = SE_Module()(input=x, identity=input)
-        else:
-            x = self.Conv2D_block(x, num_feature, kernel=1, use_skip=True, identity=input)
+
+        elif self.module == 'cbam':
+            x = self.Conv2D_block(x, num_feature, kernel=1)
+            identity = x
+            x = self.CBAM(x)
+            x = Add()([x, identity])
 
         return x
 
@@ -105,7 +161,7 @@ class Res2Net50(Res2NetModel):
     """
     Implementation of Res2Net-50 architecture.
     """
-    def __init__(self, image_size, num_classes, scale=4, use_se=False):
+    def __init__(self, image_size, num_classes, scale=4, module=None):
         """
         Initializes the Res2Net-50 model with specified parameters.
         
@@ -115,7 +171,7 @@ class Res2Net50(Res2NetModel):
             - scale (int): The scale factor for the model.
             - use_se (bool): Whether to use Squeeze-and-Excitation (SE) blocks.
         """
-        super().__init__(image_size=image_size, num_classes=num_classes, scale=scale, use_se=use_se)
+        super().__init__(image_size=image_size, num_classes=num_classes, scale=scale, module=module)
 
     def build_model(self):
         """
@@ -151,10 +207,12 @@ class Res2Net50(Res2NetModel):
         x = GlobalAveragePooling2D()(x)
         output = Dense(self.num_classes, activation="softmax", dtype=tf.float32)(x)
 
-        if self.use_se:
+        if self.module is None:
+            model_name = f"Res2Net50_{self.image_size}x{self.image_size}_{self.num_classes}Class"
+        elif self.module == 'se':
             model_name = f"Res2Net50SE_{self.image_size}x{self.image_size}_{self.num_classes}Class"
-        else:
-            model_name = (f"Res2Net50_{self.image_size}x{self.image_size}_{self.num_classes}Class")
+        elif self.module == 'cbam':
+            model_name = f"Res2Net50CBAM_{self.image_size}x{self.image_size}_{self.num_classes}Class"
 
         model = Model(inputs=[input], outputs=output, name=model_name)
         return model
@@ -164,7 +222,7 @@ class Res2Net101(Res2NetModel):
     """
     Implementation of Res2Net-101 architecture.
     """
-    def __init__(self, image_size, num_classes, scale=4, use_se=False):
+    def __init__(self, image_size, num_classes, scale=4, module=None):
         """
         Initializes the Res2Net-101 model with specified parameters.
         
@@ -174,7 +232,7 @@ class Res2Net101(Res2NetModel):
             - scale (int): The scale factor for the model.
             - use_se (bool): Whether to use Squeeze-and-Excitation (SE) blocks.
         """
-        super().__init__(image_size=image_size, num_classes=num_classes, scale=scale, use_se=use_se)
+        super().__init__(image_size=image_size, num_classes=num_classes, scale=scale, module=module)
 
     def build_model(self):
         """
@@ -210,10 +268,12 @@ class Res2Net101(Res2NetModel):
         x = GlobalAveragePooling2D()(x)
         output = Dense(self.num_classes, activation="softmax", dtype=tf.float32)(x)
 
-        if self.use_se:
-            model_name = f"Res2Net101SE_{self.image_size}x{self.image_size}_{self.num_classes}Class"
-        else:
+        if self.module is None:
             model_name = f"Res2Net101_{self.image_size}x{self.image_size}_{self.num_classes}Class"
+        elif self.module == 'se':
+            model_name = f"Res2Net101SE_{self.image_size}x{self.image_size}_{self.num_classes}Class"
+        elif self.module == 'cbam':
+            model_name = f"Res2Net101CBAM_{self.image_size}x{self.image_size}_{self.num_classes}Class"
 
         model = Model(inputs=[input], outputs=output, name=model_name)
         return model
@@ -223,7 +283,7 @@ class Res2Net152(Res2NetModel):
     """
     Implementation of Res2Net-152 architecture.
     """
-    def __init__(self, image_size, num_classes, scale=4, use_se=False):
+    def __init__(self, image_size, num_classes, scale=4, module=None):
         """
         Initializes the Res2Net-152 model with specified parameters.
         
@@ -233,7 +293,7 @@ class Res2Net152(Res2NetModel):
             - scale (int): The scale factor for the model.
             - use_se (bool): Whether to use Squeeze-and-Excitation (SE) blocks.
         """
-        super().__init__(image_size=image_size, num_classes=num_classes, scale=scale, use_se=use_se)
+        super().__init__(image_size=image_size, num_classes=num_classes, scale=scale, module=module)
 
     def build_model(self):
         """
@@ -269,10 +329,12 @@ class Res2Net152(Res2NetModel):
         x = GlobalAveragePooling2D()(x)
         output = Dense(self.num_classes, activation="softmax", dtype=tf.float32)(x)
 
-        if self.use_se:
-            model_name = f"Res2Net152SE_{self.image_size}x{self.image_size}_{self.num_classes}Class"
-        else:
+        if self.module is None:
             model_name = f"Res2Net152_{self.image_size}x{self.image_size}_{self.num_classes}Class"
+        elif self.module == 'se':
+            model_name = f"Res2Net152SE_{self.image_size}x{self.image_size}_{self.num_classes}Class"
+        elif self.module == 'cbam':
+            model_name = f"Res2Net152CBAM_{self.image_size}x{self.image_size}_{self.num_classes}Class"
 
         model = Model(inputs=[input], outputs=output, name=model_name)
         return model
