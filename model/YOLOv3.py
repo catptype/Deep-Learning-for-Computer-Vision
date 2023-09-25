57e67830c84d17bc7742edb890a6d623596e0062
Cat <cattypkung@gmail.com>
2023-09-25 18:38:19 +0700

    Create YOLOv3.py

---
+++ model/YOLOv3.py
@@ -0,0 +1,100 @@
+import sys
+sys.dont_write_bytecode = True
+
+from tensorflow.keras.layers import (
+    Add,
+    BatchNormalization,
+    Concatenate,
+    Conv2D,
+    Input,
+    LeakyReLU,
+    UpSampling2D,
+)
+from tensorflow.keras.models import Model
+from .DeepLearningModel import DeepLearningModel
+
+class YOLOv3(DeepLearningModel):
+    def __init__(self, image_size, num_classes):
+        super().__init__(image_size=image_size, num_classes=num_classes)
+
+    def conv2D_block(self, input, num_feature, kernel=3, strides=1, upsampling=False):
+        x = Conv2D(num_feature, (kernel, kernel), strides=strides, padding="same", kernel_initializer="he_normal")(input)
+        x = BatchNormalization()(x)
+        x = LeakyReLU(0.1)(x)
+        if upsampling:
+            x = UpSampling2D()(x)
+        return x
+
+    def res_unit(self, input, num_feature):
+        x = self.conv2D_block(input, num_feature // 2, kernel=1)
+        x = self.conv2D_block(x, num_feature, kernel=3)
+        x = Add()([input, x])
+        return x
+    
+    def conv2D_unit(self, input, num_feature):
+        x = self.conv2D_block(input, num_feature // 2, kernel=1)
+        x = self.conv2D_block(x, num_feature)
+        x = self.conv2D_block(x, num_feature // 2, kernel=1)
+        x = self.conv2D_block(x, num_feature)
+        x = self.conv2D_block(x, num_feature // 2, kernel=1)
+        return x
+    
+    def scale_prediction(self, input, num_feature, name=None):
+        output_feature = (self.num_classes + 5) * 3
+        x = self.conv2D_block(input, num_feature)
+        x = Conv2D(output_feature, (1,1), name=name)(x)
+        return x
+    
+    def build_model(self):
+        # Input layer
+        input = Input(shape=(self.image_size, self.image_size, 3), name="Input_image")
+
+        # Darknet 53
+        # Stage 0
+        x = self.conv2D_block(input, 32)
+        x = self.conv2D_block(x, 64, strides=2)
+        x = self.res_unit(x, 64)
+
+        # Stage 1
+        x = self.conv2D_block(x, 128, strides=2)
+        for _ in range(2):
+            x = self.res_unit(x, 128)
+
+        # Stage 2
+        x = self.conv2D_block(x, 256, strides=2)
+        for _ in range(8):
+            x = self.res_unit(x, 256)
+        skip_L = x
+
+        # Stage 3
+        x = self.conv2D_block(x, 512, strides=2)
+        for _ in range(8):
+            x = self.res_unit(x, 512)
+        skip_M = x
+
+        # Stage 4
+        x = self.conv2D_block(x, 1024, strides=2)
+        for _ in range(4):
+            x = self.res_unit(x, 1024)
+
+        # YOLO small
+        x = self.conv2D_unit(x, 1024)
+        yolo_S = self.scale_prediction(x, 1024, name="output_small")
+
+        # YOLO medium
+        x = self.conv2D_block(x, 256, kernel=1, upsampling=True)
+        x = Concatenate()([x, skip_M])
+        x = self.conv2D_unit(x, 512)
+        yolo_M = self.scale_prediction(x, 512, name="output_medium")
+
+        # YOLO large
+        x = self.conv2D_block(x, 128, kernel=1, upsampling=True)
+        x = Concatenate()([x, skip_L])
+        x = self.conv2D_unit(x, 256)
+        yolo_L = self.scale_prediction(x, 256, name="output_large")
+        
+        # Output
+        model_name = f"YOLOv3_{self.image_size}x{self.image_size}_{self.num_classes}Class"
+
+        model = Model(inputs=[input], outputs=[yolo_S, yolo_M, yolo_L], name=model_name)
+        return model
