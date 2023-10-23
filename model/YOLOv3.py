1b5fa9f63dcd33ba187395f4fab6a01a5c25db78
Cat <cattypkung@gmail.com>
2023-10-23 20:43:49 +0700

    Implement loss function

--- model/YOLOv3.py
+++ model/YOLOv3.py
@@ -26,19 +26,6 @@ class YOLOv3(DeepLearningModel):
             or a tuple (width, height) for non-square images.
         num_anchor (int): The number of anchor boxes used for object detection.
         num_class (int): The number of output classes for object detection.
-
-    Methods:
-        conv2D_block(input, num_feature, kernel=3, strides=1, upsampling=False):
-            Create a 2D convolutional block with optional upsampling.
-        res_unit(input, num_feature):
-            Create a residual unit.
-        conv2D_unit(input, num_feature):
-            Create a convolutional unit.
-        scale_prediction(input, num_feature, name=None):
-            Create the prediction tensor for object detection.
-        build_model():
-            Build the YOLOv3 model architecture.
-
     """
     def __init__(self, image_size, num_anchor, num_class):
         """
@@ -56,19 +43,6 @@ class YOLOv3(DeepLearningModel):
         super().__init__()
 
     def conv2D_block(self, input, num_feature, kernel=3, strides=1, upsampling=False):
-        """
-        Create a 2D convolutional block with optional upsampling.
-
-        Parameters:
-            input: Input tensor for the convolutional block.
-            num_feature (int): The number of output feature maps.
-            kernel (int, optional): The kernel size for convolution. Default is 3.
-            strides (int, optional): The stride for convolution. Default is 1.
-            upsampling (bool, optional): Whether to apply upsampling. Default is False.
-
-        Returns:
-            TensorFlow tensor representing the output of the convolutional block.
-        """
         x = Conv2D(num_feature, (kernel, kernel), strides=strides, padding="same", kernel_initializer="he_normal")(input)
         x = BatchNormalization()(x)
         x = LeakyReLU(0.1)(x)
@@ -77,32 +51,12 @@ class YOLOv3(DeepLearningModel):
         return x
 
     def res_unit(self, input, num_feature):
-        """
-        Create a residual unit.
-
-        Parameters:
-            input: Input tensor for the residual unit.
-            num_feature (int): The number of output feature maps.
-
-        Returns:
-            TensorFlow tensor representing the output of the residual unit.
-        """
         x = self.conv2D_block(input, num_feature // 2, kernel=1)
         x = self.conv2D_block(x, num_feature, kernel=3)
         x = Add()([input, x])
         return x
     
     def conv2D_unit(self, input, num_feature):
-        """
-        Create a convolutional unit.
-
-        Parameters:
-            input: Input tensor for the convolutional unit.
-            num_feature (int): The number of output feature maps.
-
-        Returns:
-            TensorFlow tensor representing the output of the convolutional unit.
-        """
         x = self.conv2D_block(input, num_feature // 2, kernel=1)
         x = self.conv2D_block(x, num_feature)
         x = self.conv2D_block(x, num_feature // 2, kernel=1)
@@ -111,22 +65,53 @@ class YOLOv3(DeepLearningModel):
         return x
     
     def scale_prediction(self, input, num_feature, name=None):
-        """
-        Scale the prediction tensor for object detection.
-
-        Parameters:
-            input: Input tensor to be scaled.
-            num_feature (int): The number of output feature maps.
-            name (str, optional): The name for the output layer.
-
-        Returns:
-            TensorFlow tensor representing the scaled prediction.
-        """
         output_feature = (self.num_class + 5) * 3
         x = self.conv2D_block(input, num_feature)
-        x = Conv2D(output_feature, (1,1), activation="linear")(x)
-        x = Reshape((x.shape[1], x.shape[2], self.num_anchor, 5+self.num_class), name=name, dtype=tf.float32)(x)
+        x = Conv2D(output_feature, (1,1), activation="linear", dtype=tf.float32)(x)
+        x = Reshape((x.shape[1], x.shape[2], self.num_anchor, 5 + self.num_class), name=name, dtype=tf.float32)(x)
         return x
+    
+    def yolo_loss(self, y_true, y_pred):
+        # Define constants
+        bce = tf.keras.losses.BinaryCrossentropy(from_logits=True) # Set True because final layer use activation function Linear see at scale_prediction
+        mse = tf.keras.losses.MeanSquaredError()
+        cce = tf.keras.losses.CategoricalCrossentropy(from_logits=True) # Because my implemented YOLOv3DataGenerator output format is one-hot vector
+        lambda_coord = 5.0  # Weight for localization loss, YOLOv1 paper suggest for 5.0
+        lambda_noobj = 0.5  # Weight for confidence loss when no object is present, to make sure the network won’t be dominated by cells that don’t have objects.
+
+        # Extract components from y_true and y_pred based on my implemented YOLOv3DataGenerator
+        true_obj = y_true[..., 0]  # Objectness score
+        true_box = y_true[..., 1:5]  # Bounding box coordinates (x, y, width, height)
+        true_class = y_true[..., 5:]  # Class probabilities
+
+        pred_obj = y_pred[..., 0]  # Objectness score
+        pred_box = y_pred[..., 1:5]  # Bounding box coordinates (x, y, width, height)
+        pred_class = y_pred[..., 5:]  # Class probabilities
+
+        # Create numpy array mask for object and no-object
+        mask_obj = true_obj == 1  # in paper this is Iobj_ij
+        mask_noobj = true_obj == 0  # in paper this is Inoobj_ij
+
+        # Confidence loss
+        obj_loss = bce(true_obj[mask_obj], pred_obj[mask_obj])
+        no_obj_loss = bce(true_obj[mask_noobj], pred_obj[mask_noobj])
+        confidence_loss = obj_loss + (lambda_noobj * no_obj_loss)
+
+        # Box coordinates loss
+        box_loss = mse(true_box[mask_obj], pred_box[mask_obj])
+        box_loss *= lambda_coord
+
+        # Classification loss
+        class_loss = cce(true_class[mask_obj], pred_class[mask_obj])
+
+        # Calculate the total YOLOv3 loss
+        total_loss = confidence_loss + box_loss + class_loss
+
+        return total_loss
+    
+    # Override compile function
+    def compile(self, optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4)):
+        self.model.compile(optimizer=optimizer, loss=self.yolo_loss, metrics=["accuracy"])
     
     def build_model(self):
         # Input layer
@@ -171,19 +156,19 @@ class YOLOv3(DeepLearningModel):
 
         # YOLO small
         x = self.conv2D_unit(x, 1024)
-        yolo_S = self.scale_prediction(x, 1024, name="output_small")
+        yolo_S = self.scale_prediction(x, 1024, name="small")
 
         # YOLO medium
         x = self.conv2D_block(x, 256, kernel=1, upsampling=True)
         x = Concatenate()([x, skip_M])
         x = self.conv2D_unit(x, 512)
-        yolo_M = self.scale_prediction(x, 512, name="output_medium")
+        yolo_M = self.scale_prediction(x, 512, name="medium")
 
         # YOLO large
         x = self.conv2D_block(x, 128, kernel=1, upsampling=True)
         x = Concatenate()([x, skip_L])
         x = self.conv2D_unit(x, 256)
-        yolo_L = self.scale_prediction(x, 256, name="output_large")
+        yolo_L = self.scale_prediction(x, 256, name="large")
         
         # Output
         model = Model(inputs=[input], outputs=[yolo_S, yolo_M, yolo_L], name=model_name)
