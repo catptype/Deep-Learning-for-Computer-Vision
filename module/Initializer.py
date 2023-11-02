e04903f2bc2cedb07eacf03ac6b9b2486a1e668e
Cat <cattypkung@gmail.com>
2023-11-02 18:56:46 +0700

    Create Initializer.py

---
+++ module/Initializer.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+class Initializer:
+
+    @staticmethod
+    def mixed_precision16():
+        tf.keras.mixed_precision.set_global_policy("mixed_float16")
+
+    @staticmethod
+    def memory_growth():
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus: 
+            tf.config.experimental.set_memory_growth(gpu, True)
