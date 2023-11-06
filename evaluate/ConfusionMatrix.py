a9e47d89213dc883b208ec0b2cd0627212e894ca
Cat <cattypkung@gmail.com>
2023-11-06 15:48:41 +0700

    Rename file
    - Rename from "ImageClassificationReporter.py" to "ConfusionMatrix.py"

--- evaluate/ConfusionMatrix.py
+++ evaluate/ConfusionMatrix.py
@@ -8,16 +8,11 @@ import tensorflow as tf
 from model.ImportModel import ImportModel
 from sklearn.metrics import confusion_matrix, classification_report
 
-# Avoid out of memory errors by setting GPU Memory Consumption Growth
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus: 
-    tf.config.experimental.set_memory_growth(gpu, True)
-
 # Suppress log below than ERROR
 # such INFO:tensorflow:Mixed precision compatibility check (mixed_float16): OK
 tf.get_logger().setLevel('ERROR')
 
-class ImageClassificationReporter():
+class ConfusionMatrix:
     """
     Image Classification Reporter.
 
@@ -32,7 +27,7 @@ class ImageClassificationReporter():
         figure_report (matplotlib.figure.Figure): The visual representation of the confusion matrix as a matplotlib figure.
 
     """
-    def __init__(self, h5_file, data_list, label_list, image_processor, model_name=None, force_batch=None):
+    def __init__(self, h5_file, data_list, label_list, model_name=None, force_batch=None):
         """
         Initialize the ImageClassificationReporter instance.
 
@@ -40,19 +35,15 @@ class ImageClassificationReporter():
             h5_file (str): The file path of the pre-trained image classification model in ".h5" format.
             data_list (list): A list of tuples containing image file paths and their corresponding labels.
             label_list (list): A list of labels for classification.
-            image_processor (function): A function for preprocessing images.
             model_name (str, optional): An optional name for the model. If not provided, the name from the loaded model is used.
             force_batch (int, optional): An optional batch size for predictions. If provided, predictions are made in batches.
-            
-        Raises:
-            ValueError: If the provided file path does not have a ".h5" extension.
         """
         if not h5_file.endswith(".h5"):
             raise ValueError("Invalid: The h5_file must have a .h5 extension.")
         
         self.__model = ImportModel(h5_file)
         self.model_name = self.__model.model.name if model_name is None else model_name
-        self.__image_processor = image_processor
+        self.__input_shape = self.__model.get_input_shape()
         self.__image_path = [image for image, _ in data_list]
         self.__true_label = [label for _, label in data_list]
         self.__label_list = label_list
@@ -62,21 +53,24 @@ class ImageClassificationReporter():
         self.figure_report = self.__visualize_report()
 
     # Private methods
+    @tf.autograph.experimental.do_not_convert
+    def __image_resize(self, image_path):
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_image(image, expand_animations=False)
+        image = tf.image.resize(image, (self.__input_shape[0], self.__input_shape[1]),
+                                preserve_aspect_ratio=True,
+                                antialias=True)
+        image = tf.image.resize_with_pad(image, self.__input_shape[0], self.__input_shape[1])
+        image = image / 255.0
+        
+        return image
+
     def __predict(self, batch_size):
-        """
-        Perform predictions on input data.
-
-        Args:
-            batch_size (int): The batch size for making predictions.
-
-        Returns:
-            list: A list of predicted labels.
-        """
         print("Processing class prediction ...")
         prediction_result = []
 
-        batch_image = tf.data.Dataset.from_tensor_slices((self.__image_path, [self.__model.image_size] * len(self.__image_path)))
-        batch_image = batch_image.map(self.__image_processor)
+        batch_image = tf.data.Dataset.from_tensor_slices(self.__image_path)
+        batch_image = batch_image.map(self.__image_resize)
         batch_image = batch_image.batch(batch_size)
 
         progress_bar = tf.keras.utils.Progbar(len(batch_image), 
@@ -98,36 +92,18 @@ class ImageClassificationReporter():
         return prediction_result
 
     def __generate_confusion_matrix(self):
-        """
-        Generate the confusion matrix based on true and predicted labels.
-
-        Returns:
-            numpy.ndarray: The confusion matrix.
-        """
         print("Generating confusion matrix ... ", end="")
         matrix = confusion_matrix(self.__true_label, self.__predict_label)
         print("Complete")
         return matrix
 
     def __generate_text_report(self):
-        """
-        Generate the text-based classification report.
-
-        Returns:
-            str: The text-based classification report.
-        """
         print("Generating text based classification report ... ", end="")
         report = classification_report(self.__true_label, self.__predict_label)
         print("Complete")
         return report
 
     def __visualize_report(self):
-        """
-        Visualize the confusion matrix as a matplotlib figure.
-
-        Returns:
-            matplotlib.figure.Figure: The visual representation of the confusion matrix.
-        """
         # Creating a new figure
         fig = plt.figure(figsize=(8,6))
         
