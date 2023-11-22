08f05e4b516a2a905f6ef612d99b2cff8af1300c
Cat <cattypkung@gmail.com>
2023-11-22 14:12:55 +0700

    Merge code

--- module/utility/ImageSimilarity.py
+++ module/utility/ImageSimilarity.py
@@ -1,7 +1,7 @@
 import json
 import random
 import numpy as np
-from .VectorCalculator import VectorCalculator
+from numpy.linalg import norm
 
 class ImageSimilarity:
 
@@ -37,3 +37,21 @@ class ImageSimilarity:
         except:
             print(f"Your JSON file contains '{len(image_path_list)}' images.")
             return []
+
+class VectorCalculator:
+
+    @staticmethod
+    def euclidean_distance(query_vector, dataset_vector):
+        if len(query_vector) != len(dataset_vector[0]):
+            raise ValueError("Query vector and dataset vectors must have the same length")
+    
+        distances = norm(dataset_vector - query_vector, axis=1)
+        return distances
+
+    @staticmethod
+    def cosine_similarity(query_vector, dataset_vector):
+        if len(query_vector) != len(dataset_vector[0]):
+            raise ValueError("Query vector and dataset vectors must have the same length")
+        
+        cosine_list = np.dot(dataset_vector, query_vector) / (norm(dataset_vector, axis=1) * norm(query_vector))
+        return cosine_list
