1425d7be10d88e3f068d694831f25f14abf10d13
Cat <cattypkung@gmail.com>
2023-12-05 16:53:12 +0700

    Update Initializer.py

--- module/utility/Initializer.py
+++ module/utility/Initializer.py
@@ -12,6 +12,18 @@ class Initializer:
         gpus = tf.config.experimental.list_physical_devices('GPU')
         for gpu in gpus: 
             tf.config.experimental.set_memory_growth(gpu, True)
+    
+    @staticmethod
+    def set_gpu_memory_limit(MB=1024):
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, False)
+            tf.config.experimental.set_virtual_device_configuration(
+                gpu,
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=int(MB))]
+            )
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 
     @staticmethod
     def show_version():
